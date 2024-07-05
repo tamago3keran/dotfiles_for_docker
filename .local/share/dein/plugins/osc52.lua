04f296748edfb975bf98b8041337863ce30ab318
tamago3keran <yamataku3831@gmail.com>
2024-07-05 13:49:42 +0000

    modified osc52 config

--- .local/share/dein/plugins/osc52.lua
+++ .local/share/dein/plugins/osc52.lua
@@ -1,7 +1,21 @@
-function copy()
-  require('osc52').copy_register('+')
+require('osc52').setup {}
+
+local function copy(lines, _)
+    require('osc52').copy(table.concat(lines, '\n'))
 end
 
-vim.api.nvim_create_autocmd('TextYankPost', {callback = copy})
+vim.g.clipboard = {
+    name = 'osc52',
+    copy = {['+'] = copy, ['*'] = copy},
+    paste = {['+'] = '', ['*'] = ''},
+}
 
-require('osc52').setup {}
+vim.api.nvim_create_autocmd('TextYankPost', {
+    group = vim.api.nvim_create_augroup('Yank', {clear = true}),
+    pattern = '*',
+    callback = function()
+        if vim.v.event.operator == 'y' and vim.v.event.regname == '' then
+            require('osc52').copy_register ''
+        end
+    end,
+})
