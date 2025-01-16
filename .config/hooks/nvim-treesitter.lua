3cc35c4c62aa734b2b86b24785276b2429e3c250
tamago3keran <yamataku3831@gmail.com>
2025-01-16 13:10:29 +0000

    modified config of treesitter for react

--- .config/hooks/nvim-treesitter.lua
+++ .config/hooks/nvim-treesitter.lua
@@ -1,6 +1,6 @@
 -- lua_source {{{
 require('nvim-treesitter.configs').setup({
-  ensure_installed = { "typescript", "lua", "bash" },
+  ensure_installed = { "typescript", "javascript", "tsx" },
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = false,
