f3098c5e541921c168975555060fabcf9ab31b3c
tamago3keran <yamataku3831@gmail.com>
2025-01-16 13:17:36 +0000

    modified config of treesitter for python

--- .config/hooks/nvim-treesitter.lua
+++ .config/hooks/nvim-treesitter.lua
@@ -1,6 +1,6 @@
 -- lua_source {{{
 require('nvim-treesitter.configs').setup({
-  ensure_installed = { "typescript", "lua", "bash" },
+  ensure_installed = { "python" },
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = false,
