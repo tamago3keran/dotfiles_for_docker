fa15d51454aa5516fb07ae3f79125d3124d8cb2b
tamago3keran <yamataku3831@gmail.com>
2025-01-19 08:44:08 +0000

    exclude ddu-filer from tabby

--- .config/hooks/tabby.lua
+++ .config/hooks/tabby.lua
@@ -5,6 +5,13 @@ local cwd = function()
   return " 󰚌 " .. vim.fn.fnamemodify(vim.fn.getcwd(), ":t") .. " "
 end
 
+local excluded_files = { "ddu-filer-filer" }
+local function is_excluded(winid)
+  local bufid = vim.api.nvim_win_get_buf(winid)
+  local fname = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(bufid), ":t")
+  return vim.tbl_contains(excluded_files, fname)
+end
+
 local line = {
   hl = "UserCLFill",
   layout = "active_wins_at_tail",
@@ -29,6 +36,9 @@ local line = {
   },
   top_win = {
     label = function(winid)
+      if is_excluded(winid) then
+        return { "" }
+      end
       return {
         "  " .. filename.unique(winid) .. " ",
         hl = "TabLine",
@@ -37,6 +47,9 @@ local line = {
   },
   win = {
     label = function(winid)
+      if is_excluded(winid) then
+        return { "" }
+      end
       return {
         "  " .. filename.unique(winid) .. " ",
         hl = "TabLine",
