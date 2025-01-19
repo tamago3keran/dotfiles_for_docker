1001bafd489410b5b9aa49595f06ec9734341db0
tamago3keran <yamataku3831@gmail.com>
2025-01-19 14:15:55 +0000

    code format of init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -8,16 +8,16 @@ vim.opt.expandtab = true
 vim.opt.splitright = true
 vim.opt.winblend = 15
 
-vim.env.CONFIG_DIR = os.getenv("HOME") .. "/.config"
-vim.env.HOOKS_DIR = os.getenv("HOME") .. "/.config/hooks"
-vim.env.TOMLS_DIR = os.getenv("HOME") .. "/.config/tomls"
-vim.env.CACHE_DIR = os.getenv("HOME") .. "/.cache"
+vim.env.CONFIG_DIR = os.getenv('HOME') .. '/.config'
+vim.env.HOOKS_DIR = os.getenv('HOME') .. '/.config/hooks'
+vim.env.TOMLS_DIR = os.getenv('HOME') .. '/.config/tomls'
+vim.env.CACHE_DIR = os.getenv('HOME') .. '/.cache'
 
 vim.g.floating_window_with_preview_config = {
   margin_left = math.floor(vim.o.columns * 0.1),
   margin_top = math.floor(vim.o.lines * 0.1),
   width = math.floor(vim.o.columns * 0.4),
-  height = math.floor(vim.o.lines * 0.8)
+  height = math.floor(vim.o.lines * 0.8),
 }
 vim.g.floating_window_config = {
   margin_left = math.floor(vim.o.columns * 0.05),
@@ -26,14 +26,14 @@ vim.g.floating_window_config = {
   height = math.floor(vim.o.lines * 0.8),
 }
 
-local dppBase = vim.env.CACHE_DIR .. "/dpp"
-local dppSrc = vim.env.CACHE_DIR .. "/dpp/repos/github.com/Shougo/dpp.vim"
-local dppLazy = vim.env.CACHE_DIR .. "/dpp/repos/github.com/Shougo/dpp-ext-lazy"
-local dppInstaller = vim.env.CACHE_DIR .. "/dpp/repos/github.com/Shougo/dpp-ext-installer"
-local dppToml = vim.env.CACHE_DIR .. "/dpp/repos/github.com/Shougo/dpp-ext-toml"
-local dppGit = vim.env.CACHE_DIR .. "/dpp/repos/github.com/Shougo/dpp-protocol-git"
-local denopsSrc = vim.env.CACHE_DIR .. "/dpp/repos/github.com/vim-denops/denops.vim"
-local dppConfig = vim.env.CONFIG_DIR .. "/dpp/dpp.ts"
+local dppBase = vim.env.CACHE_DIR .. '/dpp'
+local dppSrc = vim.env.CACHE_DIR .. '/dpp/repos/github.com/Shougo/dpp.vim'
+local dppLazy = vim.env.CACHE_DIR .. '/dpp/repos/github.com/Shougo/dpp-ext-lazy'
+local dppInstaller = vim.env.CACHE_DIR .. '/dpp/repos/github.com/Shougo/dpp-ext-installer'
+local dppToml = vim.env.CACHE_DIR .. '/dpp/repos/github.com/Shougo/dpp-ext-toml'
+local dppGit = vim.env.CACHE_DIR .. '/dpp/repos/github.com/Shougo/dpp-protocol-git'
+local denopsSrc = vim.env.CACHE_DIR .. '/dpp/repos/github.com/vim-denops/denops.vim'
+local dppConfig = vim.env.CONFIG_DIR .. '/dpp/dpp.ts'
 
 vim.opt.runtimepath:prepend(dppSrc)
 vim.opt.runtimepath:prepend(dppLazy)
@@ -42,26 +42,26 @@ vim.opt.runtimepath:prepend(dppToml)
 vim.opt.runtimepath:prepend(dppGit)
 vim.opt.runtimepath:prepend(denopsSrc)
 
-local dpp = require("dpp")
+local dpp = require('dpp')
 
 if dpp.load_state(dppBase) then
-  vim.api.nvim_create_autocmd("User", {
-    pattern = "DenopsReady",
+  vim.api.nvim_create_autocmd('User', {
+    pattern = 'DenopsReady',
     callback = function()
-      vim.notify("dpp load_state() is failed", "info", {title = "dpp.vim"})
+      vim.notify('dpp load_state() is failed', 'info', { title = 'dpp.vim' })
       dpp.make_state(dppBase, dppConfig)
     end,
   })
 else
   vim.api.nvim_create_autocmd('BufWritePost', {
-    pattern = {"*.lua", "*.vim", "*.toml", "*.ts", "vimrc", ".vimrc"},
+    pattern = { '*.lua', '*.vim', '*.toml', '*.ts', 'vimrc', '.vimrc' },
     callback = function()
       dpp.check_files()
     end,
   })
 
   vim.api.nvim_create_autocmd('BufWritePost', {
-    pattern = {"*.toml"},
+    pattern = { '*.toml' },
     callback = function()
       if 0 < #dpp.sync_ext_action('installer', 'getNotInstalled') then
         dpp.async_ext_action('installer', 'install')
@@ -70,10 +70,10 @@ else
   })
 end
 
-vim.api.nvim_create_autocmd("User", {
-  pattern = "Dpp:makeStatePost",
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'Dpp:makeStatePost',
   callback = function()
-    vim.notify("dpp make_state() is done", "info", {title = "dpp.vim"})
+    vim.notify('dpp make_state() is done', 'info', { title = 'dpp.vim' })
   end,
 })
 
@@ -88,5 +88,5 @@ vim.api.nvim_create_autocmd('BufWritePre', {
   end,
 })
 
-vim.cmd("filetype indent plugin on")
-vim.cmd("syntax on")
+vim.cmd('filetype indent plugin on')
+vim.cmd('syntax on')
