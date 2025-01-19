249daba448c576d5a4e64a62acd6162f3015c88c
tamago3keran <yamataku3831@gmail.com>
2025-01-19 14:38:08 +0000

    code format of nvim-notify.lua

--- .config/hooks/nvim-notify.lua
+++ .config/hooks/nvim-notify.lua
@@ -1,9 +1,9 @@
 -- lua_add {{{
-local notify = require("notify")
+local notify = require('notify')
 vim.notify = notify
 
 notify.setup({
-  stages = "fade_in_slide_out",
+  stages = 'fade_in_slide_out',
   timeout = 3000,
 })
 -- }}}
