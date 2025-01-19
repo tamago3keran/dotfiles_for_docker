97fc2b772ab8d69fe950d768abf0a284b2dc8056
tamago3keran <yamataku3831@gmail.com>
2025-01-19 14:40:12 +0000

    code format of tabby.lua

--- .config/hooks/tabby.lua
+++ .config/hooks/tabby.lua
@@ -1,67 +1,67 @@
 -- lua_source {{{
-local filename = require("tabby.filename")
+local filename = require('tabby.filename')
 
 local cwd = function()
-  return " 󰚌 " .. vim.fn.fnamemodify(vim.fn.getcwd(), ":t") .. " "
+  return ' 󰚌 ' .. vim.fn.fnamemodify(vim.fn.getcwd(), ':t') .. ' '
 end
 
-local excluded_files = { "ddu-filer-filer" }
+local excluded_files = { 'ddu-filer-filer' }
 local function is_excluded(winid)
   local bufid = vim.api.nvim_win_get_buf(winid)
-  local fname = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(bufid), ":t")
+  local fname = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(bufid), ':t')
   return vim.tbl_contains(excluded_files, fname)
 end
 
 local line = {
-  hl = "UserCLFill",
-  layout = "active_wins_at_tail",
+  hl = 'UserCLFill',
+  layout = 'active_wins_at_tail',
   head = {
-    { cwd, hl = "UserTLHead" },
+    { cwd, hl = 'UserTLHead' },
   },
   active_tab = {
     label = function(tabid)
       return {
-        "  " .. tabid .. " ",
-        hl = "UserTLActive",
+        '  ' .. tabid .. ' ',
+        hl = 'UserTLActive',
       }
     end,
   },
   inactive_tab = {
     label = function(tabid)
       return {
-        "  " .. tabid .. " ",
-        hl = "UserTLBoldLine",
+        '  ' .. tabid .. ' ',
+        hl = 'UserTLBoldLine',
       }
     end,
   },
   top_win = {
     label = function(winid)
       if is_excluded(winid) then
-        return { "" }
+        return { '' }
       end
       return {
-        "  " .. filename.unique(winid) .. " ",
-        hl = "TabLine",
+        '  ' .. filename.unique(winid) .. ' ',
+        hl = 'TabLine',
       }
     end,
   },
   win = {
     label = function(winid)
       if is_excluded(winid) then
-        return { "" }
+        return { '' }
       end
       return {
-        "  " .. filename.unique(winid) .. " ",
-        hl = "TabLine",
+        '  ' .. filename.unique(winid) .. ' ',
+        hl = 'TabLine',
       }
     end,
   },
   tail = {
-    { "  ", hl = "UserTLHead" },
+    { '  ', hl = 'UserTLHead' },
   },
 }
 
-require("tabby").setup {
+require('tabby').setup({
   tabline = line,
-}
+})
 -- }}}
